3d856a4b1cfcee1b0aa3fdbbd1fea5a0fc715a71
Solweg <lngouguenheim@hotmail.fr>
2024-08-25 20:56:16 +0200

    problème résolu

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -8,15 +8,15 @@ const bookController = require("../controllers/books.js");
 router.post("/", auth, multer, bookController.createBook);
 
 // Route PUT pour modifier un livre
-router.put("/:id", auth, multer, bookController.updateBook);
+router.put("/:id",auth, multer, bookController.updateBook);
 
 // Route DELETE pour supprimer un livre
 router.delete("/:id", auth, bookController.deleteBook);
 
 // Route GET pour récupérer un livre spécifique
-router.get("/:id", auth, bookController.getOneBook);
+router.get("/:id", bookController.getOneBook);
 
 // Route GET pour récupérer tous les livres
-router.get("/", auth, bookController.getAllBooks);
+router.get("/", bookController.getAllBooks);
 
 module.exports = router;
